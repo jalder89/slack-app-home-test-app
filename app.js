69f753f51c59d3d7bbb27bac5f9e7e9aff2f51fa
Jessica Barnes <wbarnes@slack-corp.com>
2023-01-09 12:36:27 -0800

    Added event listener for app home to test welcome message when home is opened and no event view exists.

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN
 });
 
+// Listen for app home opened event
+app.event('app_home_opened', async ({ event, client }) => {
+    console.log(event);
+    // Check if the event does not contain a view payload
+    if (!event.view) {
+        // Send a welcome message to the user
+        await client.chat.postMessage({
+            channel: event.user,
+            text: `Welcome home, <@${event.user}>!`
+        });
+    }
+});
+
 // Start your app
 (async () => {
     await app.start(process.env.PORT || 3000);
